def9e86c6d7bb41bb86ff8fe2c008e274f9033ff
Fayyez <fayyez@gmail.com>
2023-07-11 23:12:32 +0500

    added cypress

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,70 @@
+import { useState } from "react"
 import "./decor.css"
 export default function App() {
-  return  <>
-  <h1>Job Application Form</h1>
-  <h2>Section 1</h2>
-  <form>
-      <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="fullName" />
-      </div>
-      <br></br>
-      <div>
-          <label htmlFor="bio">Bio</label>
-          <textarea name="bio" id="bio" />
-      </div>
-      <br></br>
-      <div>
-          <label htmlFor="jobLocation">job Location</label>
-          <select id="jobLocation">
-              <option value="">Select a country</option>
-              <option value="pak">Pakistan</option>
-              <option value="US">USA</option>
-              <option value="Dubai">Dubai</option>
-          </select>
-      </div>
-      <br></br>
-      <div>
-          <label>
-              <input type="checkbox" id="terms" />
-              I agree to the terms and Conditions
-          </label>
-      </div>
-      <button>Submit</button>
-  </form>
-</>
-}
 
-/* <form className="xyz-form" data-cy="xyz">
-    <div className="form-row"  data-cy="row">
-      <label htmlFor="">List</label>
-      <input type="text"></input>
-    </div>
-    <button className="Btn-class">ADD</button>
-  </form>
-  <h1 className=""></h1> */
+  //useStates causes elements to re-render
+  const [itemToAdd, setItemToAdd] = useState("")
+  const [toDo, setToDo] = useState([])
+
+  function submit(e) {
+
+    e.preventDefault()//prevents page from refreshing
+    setToDo((currentList) => {
+      return [
+        ...currentList,
+        {
+          id: crypto.randomUUID(), title: itemToAdd, completed: false
+        }
+      ]
+    })
+
+    setItemToAdd("")
+
+  }
+
+  function toggleToDo(id, completed) {
+    setToDo(currentList=>{
+      return currentList.map(todo => {
+        if (todo.id===id) {
+          return { ...todo,completed}
+        }
+        return toDo//if id not matched
+      })
+    })
+  }
+
+  function deleteItem(id) {
+    setToDo(currentList=>{
+      //return list of all todo items except when passed id matched item.id:
+      return currentList.filter(toDo=>toDo.id!=id)
+    })
+  }
+  return <>
+    <form className="xyz-form" data-cy="xyz" onSubmit={submit}>
+      <div className="form-row" data-cy="row">
+        <label htmlFor="new-item">List</label>
+        <input type="text" id="new-item" value={itemToAdd}
+          onChange={e => setItemToAdd(e.target.value)}></input>
+      </div>
+      <button className="Btn-class" data-cy="btn">ADD</button>
+    </form>
+    <h1 className="header">Things To Do:</h1>
+    <ul className="list-class" data-cy="list">
+    {toDo.map(toDo=> {
+      //returning an array
+        return (
+        <li key={toDo.id}>
+        <label>
+          <input type="checkbox"
+           checked={toDo.completed} onChange={e => toggleToDo(toDo.id, e.target.checked)}></input>
+          {toDo.title}
+        </label>
+        <button className="del-btn-class"
+         data-cy="del-btn"
+         onClick={()=>deleteItem(toDo.id)}>DEL!</button>
+      </li>
+      )
+      })}
+    </ul>
+  </>
+}
